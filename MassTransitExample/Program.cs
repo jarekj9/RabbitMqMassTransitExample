583bcfb6d951b13ec6c89b36eb35423af52ddcc8
Jaroslaw <jaroslaw.jankun@gmail.com>
2024-07-19 14:36:22 +0200

    add tls config

--- MassTransitExample/Program.cs
+++ MassTransitExample/Program.cs
@@ -3,6 +3,11 @@ using MassTransitExample.Abstract;
 using MassTransitExample.Consumers;
 using MassTransitExample.Events;
 using MassTransitExample.Services;
+using System.Net;
+using static MassTransit.Logging.DiagnosticHeaders.Messaging;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,16 +20,52 @@ var builder = WebApplication.CreateBuilder(args);
 ////////////////////////// for Masstransit
 builder.Services.AddScoped<IProducerService, ProducerService>();
 var queuePrefix = "tenant1";
+
+
+// Masstransit Non-TLS:
+//builder.Services.AddMassTransit(config =>
+//{
+//    config.AddConsumer<TestEventConsumer>();
+//    config.UsingRabbitMq((context, cfg) =>
+//    {
+//        cfg.Host(builder.Configuration.GetSection("EventBus")["ConnectionString"]);
+//        cfg.ReceiveEndpoint($"{queuePrefix}_{nameof(TestEvent)}", c => { c.ConfigureConsumer<TestEventConsumer>(context); });
+
+//    });
+//});
+
+
+// Masstransit TLS:
+ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
 builder.Services.AddMassTransit(config =>
 {
     config.AddConsumer<TestEventConsumer>();
     config.UsingRabbitMq((context, cfg) =>
     {
-        cfg.Host(builder.Configuration.GetSection("EventBus")["ConnectionString"]);
+        config.AddConsumer<TestEventConsumer>();
+        var rabbitMqTlsConfig = builder.Configuration.GetSection("RabbitMqTlsConfig");
+        cfg.Host(new Uri(rabbitMqTlsConfig["RabbitMqRootUri"]), h =>
+        {
+            h.Username(rabbitMqTlsConfig["UserName"]);
+            h.Password(rabbitMqTlsConfig["Password"]);
+            h.UseSsl(s =>
+            {
+                s.Protocol = SslProtocols.Tls12;
+                s.ServerName = rabbitMqTlsConfig["ServerCertCommonName"];
+                s.AllowPolicyErrors(SslPolicyErrors.RemoteCertificateChainErrors);
+                s.Certificate = GetCertificate("Properties/certificates/client_certificate.pem", "sdfsfsdfasf2348fv983298423krdsfv889893423jhfewjsdf324");
+            });
+        });
         cfg.ReceiveEndpoint($"{queuePrefix}_{nameof(TestEvent)}", c => { c.ConfigureConsumer<TestEventConsumer>(context); });
 
     });
 });
+
+static X509Certificate2 GetCertificate(string pemFilePath, string password)
+{
+    X509Certificate2 cer = new X509Certificate2(pemFilePath, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+    return cer;
+}
 //////////////////////////
 
 
