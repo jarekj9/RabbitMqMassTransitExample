ece9b67fc92457c4b00c2974753368fe68ca4f54
JJ <j-jan@outlook.com>
2025-02-17 13:27:46 +0100

    extract common rabbitmq conf for tls and non tls into separate service extension

---
+++ MassTransitExample/Extensions/ServiceExtensions.cs
@@ -0,0 +1,59 @@
+﻿using MassTransit;
+using MassTransitExample.Consumers;
+using MassTransitExample.Events;
+using System.Net.Security;
+using System.Security.Authentication;
+using System.Security.Cryptography.X509Certificates;
+
+namespace MassTransitExample.Extensions
+{
+    public static class MassTransitServiceExtensions
+    {
+        public static void AddMassTransitService(this IServiceCollection serviceCollection, IConfiguration configuration)
+        {
+            var queuePrefix = "tenant1";
+            var rabbitMqConfig = configuration.GetSection("RabbitMqConfig");
+
+            serviceCollection.AddMassTransit(config =>
+            {
+                config.AddConsumer<TestEventConsumer>();
+                config.UsingRabbitMq((context, cfg) =>
+                {
+                    ConfigureRabbitMqAuth(cfg, rabbitMqConfig);
+                    cfg.ReceiveEndpoint($"{queuePrefix}_{nameof(TestEvent)}", c => { c.ConfigureConsumer<TestEventConsumer>(context); });
+
+                });
+            });
+
+        }
+        private static X509Certificate2 GetCertificate(string pemFilePath, string password) =>
+            new X509Certificate2(pemFilePath, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
+
+        private static void ConfigureRabbitMqAuth(IRabbitMqBusFactoryConfigurator cfg, IConfigurationSection rabbitMqConfig)
+        {
+            if (rabbitMqConfig.GetValue<bool>("UseTls"))
+            {
+                cfg.Host(new Uri(rabbitMqConfig["RabbitMqRootUriTLS"]), h =>
+                {
+                    h.Username(rabbitMqConfig["UserName"]);
+                    h.Password(rabbitMqConfig["Password"]);
+                    h.UseSsl(s =>
+                    {
+                        s.Protocol = SslProtocols.Tls12;
+                        s.ServerName = rabbitMqConfig["ServerCertCommonName"];
+                        s.AllowPolicyErrors(SslPolicyErrors.RemoteCertificateChainErrors);
+                        s.Certificate = GetCertificate(rabbitMqConfig["ClientCertPath"], rabbitMqConfig["ClientCertPassword"]);
+                    });
+                });
+            }
+            else
+            {
+                cfg.Host(new Uri(rabbitMqConfig["RabbitMqRootUri"]), h =>
+                {
+                    h.Username(rabbitMqConfig["UserName"]);
+                    h.Password(rabbitMqConfig["Password"]);
+                });
+            }
+        }
+    }
+}
--- MassTransitExample/Program.cs
+++ MassTransitExample/Program.cs
@@ -2,29 +2,20 @@ using MassTransit;
 using MassTransitExample.Abstract;
 using MassTransitExample.Consumers;
 using MassTransitExample.Events;
+using MassTransitExample.Extensions;
 using MassTransitExample.Services;
-using System.Net;
-using static MassTransit.Logging.DiagnosticHeaders.Messaging;
-using System.Net.Security;
-using System.Security.Authentication;
-using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
-
-//builder.Services.AddControllers();
-//builder.Services.AddEndpointsApiExplorer();
-//builder.Services.AddSwaggerGen();
-
 
 
 ////////////////////////// for Masstransit
 builder.Services.AddScoped<IProducerService, ProducerService>();
-var queuePrefix = "tenant1";
 
 
 // Masstransit Non-TLS with connection string:
 //builder.Services.AddMassTransit(config =>
 //{
+//    var queuePrefix = "tenant1";
 //    config.AddConsumer<TestEventConsumer>();
 //    config.UsingRabbitMq((context, cfg) =>
 //    {
@@ -36,58 +27,11 @@ var queuePrefix = "tenant1";
 
 
 // Masstransit TLS with option to disable:
-ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
-builder.Services.AddMassTransit(config =>
-{
-    config.AddConsumer<TestEventConsumer>();
-    config.UsingRabbitMq((context, cfg) =>
-    {
-        config.AddConsumer<TestEventConsumer>();
-        var rabbitMqTlsConfig = builder.Configuration.GetSection("RabbitMqTlsConfig");
-        if (rabbitMqTlsConfig.GetValue<bool>("UseTls"))
-        { 
-            cfg.Host(new Uri(rabbitMqTlsConfig["RabbitMqRootUri"]), h =>
-            {
-                h.Username(rabbitMqTlsConfig["UserName"]);
-                h.Password(rabbitMqTlsConfig["Password"]);
-                h.UseSsl(s =>
-                {
-                    s.Protocol = SslProtocols.Tls12;
-                    s.ServerName = rabbitMqTlsConfig["ServerCertCommonName"];
-                    s.AllowPolicyErrors(SslPolicyErrors.RemoteCertificateChainErrors);
-                    s.Certificate = GetCertificate(rabbitMqTlsConfig["ClientCertPath"], rabbitMqTlsConfig["ClientCertPassword"]);
-                });
-            });
-        }
-        else
-        {
-            cfg.Host(new Uri(rabbitMqTlsConfig["RabbitMqRootUri"]), h =>
-            {
-                h.Username(rabbitMqTlsConfig["UserName"]);
-                h.Password(rabbitMqTlsConfig["Password"]);
-            });
-        }
-        cfg.ReceiveEndpoint($"{queuePrefix}_{nameof(TestEvent)}", c => { c.ConfigureConsumer<TestEventConsumer>(context); });
-
-    });
-});
-
-static X509Certificate2 GetCertificate(string pemFilePath, string password)
-{
-    X509Certificate2 cer = new X509Certificate2(pemFilePath, password, X509KeyStorageFlags.PersistKeySet | X509KeyStorageFlags.Exportable);
-    return cer;
-}
-//////////////////////////
-
+builder.Services.AddMassTransitService(builder.Configuration);
 
 
 var app = builder.Build();
 
-//if (app.Environment.IsDevelopment())
-//{
-//   app.UseSwagger();
-//   app.UseSwaggerUI();
-//}
 
 
 ////////////////////////// for Masstransit Test publishing:
